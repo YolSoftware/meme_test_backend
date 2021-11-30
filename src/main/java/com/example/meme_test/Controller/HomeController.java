419873f18dd2732ed3d458c19b9afb4a4fd6f9f7
tyrue <kdh9426@naver.com>
2021-12-01 01:00:33 +0900

    기본 프로젝트 생성

---
+++ src/main/java/com/example/meme_test/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.meme_test.Controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.logging.Logger;
+
+@Controller
+public class HomeController
+{
+    @GetMapping("/")
+    public String home() { return "home"; }
+}
