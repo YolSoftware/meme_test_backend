59e7056b518284be9fe0001cde8b612d04a9c0c3
tyrue <kdh9426@naver.com>
2021-11-30 23:44:52 +0900

    기본 프로젝트 생성

---
+++ src/main/java/com/example/meme_test/MemeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.meme_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meme_test/MemeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meme_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
